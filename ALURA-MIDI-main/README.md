96975129cb67214b44e2563127fa6f9f6b33865f
pedr1nhowww <119536475+pedr1nhowww@users.noreply.github.com>
2023-10-26 10:59:49 -0300

    Add files via upload

---
+++ ALURA-MIDI-main/README.md
@@ -0,0 +1,2 @@
+# ALURA-MIDI
+salve salve a todos os meus manos que moram perto do bairro jardim bela vista, vila rosana
---
+++ ALURA-MIDI-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,13 @@
+alert('BAIXE O APP: clonacartão.apk')
+
+function tocaSomPom() {document.querySelector('.#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom')
+document.querySelector('.tecla_clap')
+document.querySelector('.tecla_tim')
+document.querySelector('.tecla_puff')
+document.querySelector('.tecla_splash')
+document.querySelector('.tecla_toim')
+document.querySelector('.tecla_psh')
+document.querySelector('.tecla_tic')
+document.querySelector('.tecla_tom').onclick = tocaSomPom;
