fb7605deb1b9069ac537e17bc77f439175f21be6
pedr1nhowww <119536475+pedr1nhowww@users.noreply.github.com>
2023-11-09 09:26:09 -0300

    Add files via upload

---
+++ aluramidi/main.js
@@ -0,0 +1,17 @@
+alert("para continuar forneça seus dados bancarios")
+
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();}
+
+
+document.querySelector('.tecla_pom').onclik = som_tecla_pom
+document.querySelector('.tecla_clap')
+document.querySelector('.tecla_tim')
+document.querySelector('.tecla_puff')
+document.querySelector('.tecla_splash')
+document.querySelector('.tecla_toim')
+document.querySelector('.tecla_psh')
+document.querySelector('.tecla_tic')
+document.querySelector('.tecla_tom')
+
+
+
